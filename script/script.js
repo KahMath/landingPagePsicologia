605c7bd4eab89362daf1371771b1c5a2ef69970e
Karolina Mathias <160190348+KahMath@users.noreply.github.com>
2024-09-15 18:19:49 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,13 +1,9 @@
 const form = document.querySelector("form");
 const nome = document.getElementById("nome");
 const email = document.getElementById("email");
-const mensagem = document.getElementById("mensagem");
 const nomeErro = document.querySelector("#nome + span.erro");
 const emailErro = document.querySelector("#email + span.erro");
-const mensagemErro = document.querySelector("#mensagem + span.erro");
-const botao = document.getElementById("botao");
-const paragrafo = document.querySelector("p");
-
+// const botao = document.getElementById("botao");
 // const menuBotao = document.getElementById("botaoMenu");
 // const navBotao = document.getElementById("botaoNav");
 
@@ -33,24 +29,12 @@ email.addEventListener("input", (event) => {
     }
 })
 
-mensagem.addEventListener("input", (event) => {
-  if (mensagem.validity.valid) {
-      mensagemErro.textContent = "";
-      mensagemErro.className = "erro";
-  } else {
-      mostrarErro(mensagem, mensagemErro);
-  }
-});
-
 form.addEventListener("submit", (event) => {
     if (!nome.validity.valid) {
         mostrarErro(nome, nomeErro);
         event.preventDefault();
     } else if (!email.validity.valid) {
         mostrarErro(email, emailErro);
-        event.preventDefault();
-    } else if (!mensagem.validity.valid){
-        mostrarErro(mensagem, mensagemErro);
         event.preventDefault();
     }
 });
@@ -61,15 +45,11 @@ function mostrarErro(input, erroElement) {
             erroElement.textContent = `Você precisa inserir um ${input.id}.`;
         } else if (input.id === "email") {
             erroElement.textContent = 'Você precisa inserir um e-mail.';
-        } else if (input.id === "mensagem") {
-          erroElement.textContent = 'Você precisa inserir uma mensagem.';
         }
     } else if (input.validity.typeMismatch) {
         erroElement.textContent = 'Formato de e-mail inválido.';
     } else if (input.validity.tooShort) {
         if (input.id === "nome") {
-            erroElement.textContent = `Nome precisa ter no mínimo ${input.minLength} caracteres.`;
-        } else if (input.id === "mensagem") {
             erroElement.textContent = `Nome precisa ter no mínimo ${input.minLength} caracteres.`;
         }
     }
@@ -77,14 +57,10 @@ function mostrarErro(input, erroElement) {
     erroElement.className = "erro ativo";
 }
 
-botao.addEventListener("click", envio);
-
 function envio() {
     if (email.validity.valueMissing) {
         emailErro.textContent = "Você precisa inserir um email.";
     } else if (email.validity.typeMismatch) {
         emailErro.textContent = 'Formato de e-mail inválido.';
-    } else {
-        paragrafo.textContent = "Mensagem enviada!";
     }
 }
